3c87c0a5141c5780a520db037864ff8626e45e79
Paul Conte <bright@drizzle.net>
2015-03-10 19:57:43 -0700

    add materialize-css icons

--- components/header.jsx
+++ components/header.jsx
@@ -5,7 +5,16 @@ var Header = React.createClass({
     render: function () {
         return (
             <div className="header-container">
-                <a href="">Toggle Nav</a>
+                <nav>
+                    <div className="nav-wrapper">
+                        <ul className="center">
+                            <li><a href="sass.html"><i className="mdi-action-search"></i></a></li>
+                            <li><a href="components.html"><i className="mdi-action-view-module"></i></a></li>
+                            <li><a href="javascript.html"><i className="mdi-navigation-refresh"></i></a></li>
+                            <li><a href="mobile.html"><i className="mdi-navigation-more-vert"></i></a></li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         );
     }

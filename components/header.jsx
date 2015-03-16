00684bc99a3ee0fc3067bf9b339339f03265ddeb
Paul Conte <bright@drizzle.net>
2015-03-16 13:01:40 -0700

    move leftnav to header component, toggle leftnav when hamburger is clicked

--- components/header.jsx
+++ components/header.jsx
@@ -1,14 +1,36 @@
 var React = require('react');
 var mui = require('material-ui');
+var LeftNav = mui.LeftNav;
+var MenuItem = mui.MenuItem;
+var IconButton = mui.IconButton;
+
+var menuItems = [
+      { route: 'get-started', text: 'Get Started' },
+      { route: 'css-framework', text: 'CSS Framework' },
+      { route: 'components', text: 'Components' },
+      { type: MenuItem.Types.SUBHEADER, text: 'Resources' },
+      {
+         type: MenuItem.Types.LINK,
+         payload: 'https://github.com/callemall/material-ui',
+         text: 'GitHub'
+      }
+    ];
 
 var Header = React.createClass({
+    toggleLeftNav: function () {
+        this.refs.leftNav.toggle();
+    },
+
     render: function () {
         return (
             <div className="header-container green">
+                <LeftNav menuItems={menuItems} docked={false} ref="leftNav" />
                 <nav>
                     <div className="nav-wrapper green">
                         <ul className="left">
-                            <li><a href="#"><i className="mdi-action-list"></i></a></li>
+                            <li>
+                                <IconButton iconClassName="mdi-action-list" onClick={this.toggleLeftNav} />
+                            </li>
                         </ul>
                         <ul className="right">
                             <li><a href="sass.html"><i className="mdi-action-search"></i></a></li>
--- components/sheet.jsx
+++ components/sheet.jsx
@@ -1,24 +1,8 @@
 var React = require('react');
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
-var mui = require('material-ui');
 var Header = require('./header.jsx');
-var Main = require('./main.jsx');
 var Footer = require('./footer.jsx');
-var LeftNav = mui.LeftNav;
-var MenuItem = mui.MenuItem;
-
-var menuItems = [
-      { route: 'get-started', text: 'Get Started' },
-      { route: 'css-framework', text: 'CSS Framework' },
-      { route: 'components', text: 'Components' },
-      { type: MenuItem.Types.SUBHEADER, text: 'Resources' },
-      {
-         type: MenuItem.Types.LINK,
-         payload: 'https://github.com/callemall/material-ui',
-         text: 'GitHub'
-      },
-    ];
 
 var Sheet = React.createClass({
     render: function () {

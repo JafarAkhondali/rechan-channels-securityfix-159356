65f65b9b06a7347eea98852b209e99a54fb22fa5
Paul Conte <bright@drizzle.net>
2015-03-16 12:23:16 -0700

    test that router links work, remove Main.jsx component since it is redundant with the react-router package"

--- components/home.jsx
+++ components/home.jsx
@@ -1,11 +1,14 @@
 var React = require('react');
 var mui = require('material-ui');
+var Router = require('react-router');
+var Link = Router.Link;
 
 var Home = React.createClass({
     render: function () {
         return (
             <div className="home-container">
-                home
+                home<br />
+                <Link to="posts">Posts</Link>
             </div>
         );
     }
